27c4b998b5e21cbd2245ffcb084913e7ec00ccab
EM0L1 <Emincan.dogan@hotmail.com>
2025-01-24 22:48:17 +0300

    Update FanForceScript

--- project/Assets/Scripts/BubblePhyiscs/FanForceScript.cs
+++ project/Assets/Scripts/BubblePhyiscs/FanForceScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FanForceScipt : MonoBehaviour
+public class FanForceScript : MonoBehaviour
 {
     [SerializeField]
     private GameObject _targetObject;
@@ -23,15 +23,7 @@ public class FanForceScipt : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Bubble"))
         {
-            Debug.Log("a");
             other.GetComponent<Rigidbody2D>().AddForce(transform.up * 10);
-            RunFan();
         }
-    }
-
-
-    private void RunFan()
-    {
-        
     }
 }
