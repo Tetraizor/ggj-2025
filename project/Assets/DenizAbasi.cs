38a523706e32b623548390172ff4cedaf86f8eb0
Ardkil <139571780+Ardkil@users.noreply.github.com>
2025-01-25 18:56:18 +0300

    DenizAnasi

---
+++ project/Assets/DenizAbasi.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DenizAbasi : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.attachedRigidbody.velocity = Vector2.zero;
+        collision.attachedRigidbody.AddForce(transform.up * 20, ForceMode2D.Impulse);
+    }
+}
