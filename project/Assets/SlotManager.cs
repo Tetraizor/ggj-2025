fabe49934da62ce85285d5952050a90053b76b5b
Ardkil <139571780+Ardkil@users.noreply.github.com>
2025-01-25 22:05:18 +0300

    asdadaFFUFUFUUFF

--- project/Assets/SlotManager.cs
+++ project/Assets/SlotManager.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 public class SlotManager : MonoBehaviour
 {
     [SerializeField] private Slot[] slots;
+    [SerializeField] private Slot[] slots_initial;
     private void Awake()
     {
-        
+        if(slots == slots_initial)
+        {
+
+        }
     }
     // Start is called before the first frame update
     void Start()
